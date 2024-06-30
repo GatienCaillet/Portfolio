a26fcedb7f3e0180fd85ff2e95cad5f889af11fe
GatienCaillet <gatien38@hotmail.fr>
2024-06-30 10:15:32 +0200

    feat: Ajout de la navbar fonctionnelle, sections non développées

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('nav a').on('click', function(e) {
+        e.preventDefault();
+        
+        var targetId = $(this).attr('href').substring(1);
+        var targetElement = $('#' + targetId);
+        
+        $('html, body').animate({
+            scrollTop: targetElement.length ? targetElement.offset().top - 70 : 0
+        }, 500);
+
+        $('.navbar-collapse').removeClass('show');
+    });
+});
+
+$(function() {
+    $('.navbar-toggler').on('click', function() {
+        $('.navbar-collapse').toggleClass('show');
+    });
+});
