ed1facf04ecb68bed7c7616f0ebb4be5f317d9ce
GatienCaillet <gatien38@hotmail.fr>
2024-07-01 09:55:48 +0200

    fix:correction navbar qui dirigait sur la mauvaise partie au format téléphone

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
     $("#primary-navbar a").on('click', function(e) {
-        e.preventDefault();
-        
-        var targetId = $(this).attr('href').substring(1);
-        var targetElement = $('#' + targetId);
-        
-        $('html, body').animate({
-            scrollTop: targetElement.length ? targetElement.offset().top - 70 : 0
-        }, 500);
-
         $('.navbar-collapse').removeClass('show');
     });
 });
